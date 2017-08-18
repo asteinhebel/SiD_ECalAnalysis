22e1cd8f3bc7f38d8c7b4d370e9fdade6bfb08a2
asteinhebel <asteinhebel151@gmail.com>
2017-08-18 13:07:25 -0700

    Update fitting.py

--- calibrationLeakageStudies/showerProfiles/fitting.py
+++ calibrationLeakageStudies/showerProfiles/fitting.py
@@ -9,36 +9,49 @@ import math
 def linearFit1(ecalMid,ecalEnd,graph,totEnArray):
     fitFn=[0]*7
     integral=[0.]*7
-    for en in range(7):
-        fitFn[en]=TF1('linFitFn'+str(en),'expo(0)',ecalMid,1700) #ecalMid defines the 5th to last ECal layer where fitting begins
-        graph[en].Fit(fitFn[en], "R" )
-        linB=fitFn[en].GetParameter(0)
-        slope=fitFn[en].GetParameter(1)
-        xInt=(1/slope)*(math.log(0.000000001)-linB) #until y=10e-10
-        integral[en]=fitFn[en].Integral(ecalEnd,xInt,1.e-12)/totEnArray[en]/(6.25/2.) #integrate up to y=10e-10 and divide by 6.25/2 mm from extrapolating from thick layers but doubling their deposits in the energy sum
+    params=[0]*7
+    roundParams=[0]*7
+    for i in range(7):
+        fitFn[i]=TF1('linFitFn'+str(i),'expo(0)',ecalMid,1700) #ecalMid defines the 5th to last ECal layer where fitting begins
+        graph[i].Fit(fitFn[i], "R" )
+        params[i]=fitFn[i].GetParameters()
+        xInt=(1/params[i][1])*(math.log(0.000000001)-params[i][0]) #until y=10e-10
+        integral[i]=fitFn[i].Integral(ecalEnd,xInt,1.e-12)/totEnArray[i]/(6.25/2.) #integrate up to y=10e-10 and divide by 6.25/2 mm from extrapolating from thick layers but doubling their deposits in the energy sum
+        roundParams[i]=[0.]*2
+        for j in range(2):
+            roundParams[i][j]="{0:.4f}".format(round(params[i][j],4))
 
-    return fitFn,integral
+    return fitFn,integral,roundParams
 
 ##################################################################################
 # for x in [X_0]
 def linearFit2(ecalMid,ecalEnd,graph,totEnArray):
     fitFn=[0]*7
     integral=[0.]*7
-    for en in range(7):
-        fitFn[en]=TF1('linFitFn'+str(en),'expo(0)',ecalMid,1700) #ecalMid defines the 5th to last ECal layer where fitting begins
-        graph[en].Fit(fitFn[en], "R" )
-        linB=fitFn[en].GetParameter(0)
-        slope=fitFn[en].GetParameter(1)
-        xInt=(1/slope)*(math.log(0.000000001)-linB) #until y=10e-10
-        integral[en]=fitFn[en].Integral(ecalEnd,xInt,1.e-12)/totEnArray[en]/(6.25/2.) #integrate up to y=10e-10 and divide by 6.25/2 mm from extrapolating from thick layers but doubling their deposits in the energy sum
+    params=[0]*7
+    roundParams=[0]*7
+    for i in range(7):
+        fitFn[i]=TF1('linFitFn'+str(i),'expo(0)',ecalMid,175) #ecalMid defines the 5th to last ECal layer where fitting begins
+        graph[i].Fit(fitFn[i], "R" )
+        params[i]=fitFn[i].GetParameters()
+        xInt=(1/params[i][1])*(math.log(0.000000001)-params[i][0]) #until y=10e-10
+#        linB=fitFn[i].GetParameter(0)
+#        slope=fitFn[i].GetParameter(1)
+#        xInt=(1/slope)*(math.log(0.000000001)-linB) #until y=10e-10
+        integral[i]=fitFn[i].Integral(ecalEnd,xInt,1.e-12)/totEnArray[i]/(5.*0.26) #integrate up to y=10e-10 and divide by 5 mm*0.26 from extrapolating from radiation lengths of thick layers
+        roundParams[i]=[0.]*2
+        for j in range(2):
+            roundParams[i][j]="{0:.4f}".format(round(params[i][j],4))
 
-    return fitFn,integral
+    return fitFn,integral,roundParams
 
 ##################################################################################
 # for x in [X_0] and y in MIP *only* - gamma distribution fit of entire ECal shower
 def gammaFit(ecalEnd,graph):
     fitFn=[0]*7
     integral=[0.]*7
+    params=[0]*7
+    roundParams=[0]*7
     xInt=(-1/0.3)*math.log(1e-11) #where y=10e-11 assuming exp(-0.3x) (x~84)
     for i in range(7):
         fitFn[i]=TF1("gammaFitFn"+str(i),"[0]*TMath::Power(x,[1])*TMath::Exp(-[2]*x)",0,70)
@@ -51,7 +64,11 @@ def gammaFit(ecalEnd,graph):
         else:
             fitFn[i].SetParameter(1,4.)
         graph[i].Fit(fitFn[i])
+        params[i]=fitFn[i].GetParameters()
         integral[i]=fitFn[i].Integral(ecalEnd,xInt,1.e-12)*50./57. #integrate up to y=10e-11 and multiply by HCal X_0/layer to get idea of HCal energy that's unaccounted for
+        roundParams[i]=[0.]*3
+        for j in range(3):
+            roundParams[i][j]="{0:.4f}".format(round(params[i][j],4))
 
-    return fitFn, integral
+    return fitFn, integral,roundParams
 #################################################################################
